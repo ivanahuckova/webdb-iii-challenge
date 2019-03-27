c5ac250e0a86c2f64b821d27768316dc104cff52
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-27 14:29:41 +0100

    Initial set up of index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js').development;
+const db = knex(knexConfig);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
