986708e535741f7ffe70f23fb3b45a7cfb676aac
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-27 15:50:43 +0100

    Set up post routes for cohorts table

--- index.js
+++ index.js
@@ -10,5 +10,19 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+// =========== POST ROUTES ========== //
+
+server.post('/api/cohorts', async (req, res) => {
+  try {
+    const id = await db('cohorts').insert(req.body);
+    const newCohort = await db('cohorts')
+      .where({ id: id[0] })
+      .first();
+    res.status(201).json(newCohort);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
